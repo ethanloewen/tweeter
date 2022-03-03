2116ef515193bac40d9e239c348d7bd9cfa0a2da
Ethan Loewen <ethanloewen@gmail.com>
2022-03-03 00:30:05 +0000

    Make functions that populate tweet html markdown from tweet objects

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
+  const createTweetElement = function(obj) {
+    let markup = `
+    <article>
+      <header>
+        <i class="fa-solid fa-user"></i>
+        <h1>${obj.user.name}</h1>
+      </header>
+      <div id="tweet-body">
+        <h2>${obj.content.text}</h2>
+      </div>
+      <footer>
+        <h3>${timeago.format(obj.created_at)}</h3>
+        <span>
+          <i class="fa-solid fa-flag fa-2xs"></i>
+          <i class="fa-solid fa-retweet fa-2xs"></i>
+          <i class="fa-solid fa-heart fa-2xs"></i>
+        </span>
+      </footer>
+    </article>
+    `;
+    const $tweet = $(markup);
+    return $tweet;
+  };
+
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $readyTweet = createTweetElement(tweet);
+      $('.tweet-container').append($readyTweet);
+    }
+  };
+
+  renderTweets(data);
+
+});
+
+
+
