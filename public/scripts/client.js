4a17b892e749a2342c14759eb5085fe8da9cb730
Ethan Loewen <ethanloewen@gmail.com>
2022-03-03 08:02:11 +0000

    Hide the error icons as soon as an acceptable tweet is submitted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -68,15 +68,21 @@ $(document).ready(function() {
     if (rawData === null || rawData === '') {
       $('#error-block h4').html('Error: Your tweet is empty!');
       $('#error-block').slideDown();
+      $('#tri1, #tri2').css({'animation': 'fadeIn 2s'});
+      $('#tri1, #tri2').css({'opacity': 1});
       return;
     }
     if (rawData.length > 140) {
       $('#error-block h4').html('Error: Your tweet has exceeded 140 characters!');
       $('#error-block').slideDown();
+      $('#tri1, #tri2').css({'animation': 'fadeIn 2s'});
+      $('#tri1, #tri2').css({'opacity': 1});
       return;
     }
 
     $('#error-block').slideUp();
+    $('#tri1, #tri2').css({'opacity': 0});
+    $('#tri1, #tri2').css({'animation': 'hide 0s'});
     $.post('/tweets', serializedData, () => {
       console.log(serializedData);
       //$('#tweet-form')[0].reset();
