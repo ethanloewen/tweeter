40d337f7b4179a443dca494d578b5564978fff17
Ethan Loewen <ethanloewen@gmail.com>
2022-03-03 01:17:32 +0000

    Finish making a function to GET tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,32 +6,6 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-
   const createTweetElement = function(obj) {
     let markup = `
     <article>
@@ -64,8 +38,6 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
-
   //sends post request to server
   $('form').submit(function(event) {
     const serializedData = $('textarea').serialize();
@@ -78,7 +50,7 @@ $(document).ready(function() {
   const loadTweets = function() {
     $.ajax('/tweets', { method: 'GET' })
     .then(function (tweets) {
-      console.log('Success: ', tweets);
+      renderTweets(tweets);
     });
   };
 
